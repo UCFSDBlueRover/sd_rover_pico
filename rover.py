77ce23d6ad3e2f87222c85385c08e6b630d37537
Joshua Kissoon <kissoonjoshua@outlook.com>
2022-03-09 13:04:16 -0500

    Add timeout retransmission

--- rover.py
+++ rover.py
@@ -18,27 +18,43 @@ class main():
     def connection(self):
         while True:
             if self.connectionState == 'CLOSED':
-                    self.msgTx('SYN')
-                    self.connectionState = 'SYN-SENT'
+                print('sent syn\n')
+                self.msgTx('SYN')
+                start = time.ticks_ms()
+                self.connectionState = 'SYN-SENT'
             elif self.connectionState == 'SYN-SENT':
                 data = self.parseMsg()
                 if data and data[2] == 'SYN':
+                    print('sent ack\n')
                     self.msgTx('ACK')
+                    start = time.ticks_ms()
                     self.connectionState = 'ESTABLISHED'
+                if time.ticks_diff(time.ticks_ms(), start) > 5000:
+                    print('5 sec timeout\n')
+                    self.msgTx('SYN')
+                    start = time.ticks_ms()
             elif self.connectionState == 'ESTABLISHED':
                 data = self.parseMsg()
                 if data:
                     if data[2] == 'FIN':
                         self.connectionState = 'CLOSE-WAIT'
                     elif data[2] == 'ACK':
-                        print('recv ack')
+                        # send telemetry
+                        print('sent ack\n')
                         self.msgTx('ACK')
                     elif data[2] == 'COM':
                         # parse command
                         print(''.join(data[3:]) + '\n')
+                        print('sent ack\n')
                         self.msgTx('ACK')
+                if time.ticks_diff(time.ticks_ms(), start) > 5000:
+                    print('5 sec timeout\n')
+                    self.msgTx('ACK')
+                    start = time.ticks_ms()
             elif self.connectionState == 'CLOSE-WAIT':
+                print('sent fin\n')
                 self.msgTx('FIN')
+                start = time.ticks_ms()
                 self.connectionState = 'LAST-ACK'
             elif self.connectionState == 'LAST-ACK':
                 data = self.parseMsg()
@@ -48,19 +64,23 @@ class main():
                     self.seqNum = 0
                     self.ackNum = 0
                     time.sleep_ms(3000)
+                if time.ticks_diff(time.ticks_ms(), start) > 5000:
+                    print('5 sec timeout\n')
+                    self.msgTx('FIN')
+                    start = time.ticks_ms()
 
     def initLora(self):
         self.uart = UART(1,baudrate=115200, tx=Pin(4), rx=Pin(5), bits=8, parity=None, stop=1, timeout=2)
         self.uart.write('AT+NETWORKID=5\r\n')
-        if self.readLora() != '+OK\r\n':
+        if self.readLora() != '+OK':
             print('Error')
         self.uart.write('AT+ADDRESS=102\r\n')
-        if self.readLora() != '+OK\r\n':
+        if self.readLora() != '+OK':
             print('Error')
 
     def readLora(self):
-        start = time.time()
-        while (time.time() - start) < 5.0: 
+        start = time.ticks_ms()
+        while time.ticks_diff(time.ticks_ms(), start) < 5000:
             line = self.read()
             if line:
                 return line
@@ -84,7 +104,7 @@ class main():
     
     def read(self):
         if self.uart.any():
-            return self.uart.readline().decode('ascii')
+            return self.uart.readline().decode().strip('\r\n')
 
     def send(self, msg):
         self.uart.write(msg.encode('ascii'))
